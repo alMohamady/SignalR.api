6108be52bffdedb0108e05d44edf53bbc4ada6f3
mohamady-div <major_2047@hotmail.com>
2025-04-10 17:32:13 +0200

    Lesson 02

---
+++ SignalR.api/Classes/NotificationHub.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalR.api.Classes
+{
+    public class NotificationHub : Hub<INotifationClient>
+    {
+        public override async Task OnConnectedAsync()
+        {
+            await Clients.Client(Context.ConnectionId).ReceiveNotification(
+                   $"Connection working fine for {Context?.User?.Identity?.Name}");
+
+            await base.OnConnectedAsync();
+        }
+
+    }
+
+    public interface INotifationClient
+    {
+        Task ReceiveNotification(string message);
+    }
+}
